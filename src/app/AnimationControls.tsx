8bc46383e715f1280c15dca7031053694b58346e
LinThitHtwe <linthit2745@gmail.com>
2024-04-21 21:00:58 +0630

    [add] useAnimationControl example

---
+++ src/app/AnimationControls.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { motion, useAnimationControls } from "framer-motion";
+import React from "react";
+
+const AnimationControls = () => {
+  const controls = useAnimationControls();
+
+  const handleClick = () => {
+    controls.start("flip");
+  };
+
+  return (
+    <div
+      style={{
+        display: "grid",
+        placeContent: "center",
+        height: "100vh",
+        gap: "0.8rem",
+      }}
+    >
+      <button onClick={handleClick} className="example-button">
+        Flip Me
+      </button>
+      <motion.div
+        style={{
+          width: 150,
+          height: 150,
+          backgroundColor: "black",
+          borderRadius: 10,
+        }}
+        variants={{
+          initial: {
+            rotate: "0deg",
+          },
+          flip: {
+            rotate: "360deg",
+          },
+        }}
+        initial="initial"
+        animate={controls}
+      ></motion.div>
+    </div>
+  );
+};
+
+export default AnimationControls;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import BasicOfMotion from "./BasicOfMotion";
 import Gestures from "./Gestures";
+import AnimationControls from "./AnimationControls";
 
 export default function Home() {
   return (
     <main>
       {/* <BasicOfMotion /> */}
-
-      <Gestures />
+      {/* <Gestures /> */}
+      <AnimationControls />
     </main>
   );
 }
